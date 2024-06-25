d51fdb6164924f181353ca567e24f6ddd8ed62a2
buenorafa <rafaelbuenooo@outlook.com>
2024-06-24 23:29:51 -0300

    Refactor data attribute in Viagem class from String to Date type
    Changed the 'data' attribute in the Viagem class from String to Date to improve date handling and consistency across the application.

--- src/modelo/Viagem.java
+++ src/modelo/Viagem.java
@@ -1,46 +1,62 @@
 package modelo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Viagem {
-	private String id;
-	private String data;
-	private Veiculo veiculo;
-	private Motorista motorista;
-	private String destino;
-	
-	
-	public Viagem(String data,Veiculo veiculo, Motorista motorista, String destino){
-		this.data = data;
-		this.veiculo = veiculo;
-		this.motorista = motorista;
-		this.destino = destino; 
-		this.id = this.data + this.motorista.getCnh() + this.veiculo.getPlaca();
-	}
-	
-	public Viagem(String data, Veiculo veiculo, Motorista motorista) {
-		this.data = data;
-		this.veiculo = veiculo;
-		this.motorista = motorista;
-		this.destino = ""; 
-		this.id = this.data + this.motorista.getCnh() + this.veiculo.getPlaca();
-	}
-	
-	public String getId(){
-		return id;
-	}	
-	public String getData() {
-		return data;
-	}
-	public Veiculo getVeiculo() {
-		return veiculo;
-	}
-	public Motorista getMotorista() {
-		return motorista;
-	}
-	public String getDestino() {
-		return destino;
-	}
-	public void setDestino(String destino) {
-		this.destino = destino;
-	}
+    private String id;
+    private Date data;
+    private Veiculo veiculo;
+    private Motorista motorista;
+    private String destino;
+
+    public Viagem(Date data, Veiculo veiculo, Motorista motorista, String destino) {
+        this.data = data;
+        this.veiculo = veiculo;
+        this.motorista = motorista;
+        this.destino = destino;
+        this.id = geraId(data, veiculo.getPlaca(), motorista.getCnh());
+    }
+
+    public static String geraId(Date data, String placa, String cnh) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyMMdd");
+        String formattedDate = dateFormatter.format(data);
+        return formattedDate + placa + cnh;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+//    public void setData(Date data) {
+//        this.data = data;
+//        this.id = geraId(data, veiculo.getPlaca(), motorista.getCnh());  // Atualiza o ID quando a data muda
+//    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+
+    // Método para obter a data como uma string no formato dd-MM-yyyy
+    public String getDataAsString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(data);
+    }
 }
