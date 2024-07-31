1b44ad4032e6077f49f95ac3221a29b39d905614
Maria Eduarda Vitorino <maria.vitorino@picsel.com.br>
2024-07-31 04:09:33 -0300

    ✨ feat: create facade methods

--- src/regras_negocio/Fachada.java
+++ src/regras_negocio/Fachada.java
@@ -4,6 +4,7 @@ package regras_negocio;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import daodb4o.DAO;
 import daodb4o.DAOMotorista;
@@ -21,7 +22,6 @@ public class Fachada {
 	private static DAOVeiculo daoveiculo = new DAOVeiculo();
 	private static DAOViagem daoviagem = new DAOViagem();
 	
-	
 	public static void inicializar() {
 		DAO.open();
 	}
@@ -30,10 +30,11 @@ public class Fachada {
 		DAO.close();
 	}
 	
-	
-	public static void criarMotorista(String cnh, String nome) throws Exception {
+
+	public static void cadastrarMotorista(String cnh, String nome) throws Exception {
 		DAO.begin();
 		Motorista motorista = daomotorista.read(cnh);
+		//Verifica se o motorista já existe e lança uma exceção caso encontre
 		if(motorista != null) {
 			DAO.rollback();
 			throw new Exception("Criar motorista - CNH já existe: " + cnh);
@@ -43,19 +44,50 @@ public class Fachada {
 		DAO.commit();
 		
 	}
+
+	public static void excluirMotorista(String cnh) throws Exception {
+		DAO.begin();
+		Motorista motorista = daomotorista.read(cnh);
+		if (motorista ==null) {
+			throw new Exception("Motorista não encontrado: " + cnh);
+		}
+		daomotorista.delete(motorista);
+		DAO.commit();
+	}
+
+	//TO-DO: ALTERAR MOTORISTA
 	
-	public static void criarVeiculo(String placa, String modelo) throws Exception {
+	public static List<Motorista> listarMotoristas(){
+		return daomotorista.readAll();
+	}
+
+	public static void cadastrarVeiculo(String placa, String modelo) throws Exception {
 		DAO.begin();
 		Veiculo veiculo = daoveiculo.read(placa);
 		if(veiculo != null) {
 			DAO.rollback();
-			throw new Exception("Criar veiculo - Placa já existe: " + placa);
+			throw new Exception("Criar veículo - Placa já existe: " + placa);
 		}
 		veiculo = new Veiculo(placa, modelo);
 		daoveiculo.create(veiculo);
-		DAO.commit();
-		
+		DAO.commit();		
 	}
+
+	public static void excluirVeiculo(String placa) throws Exception {
+		DAO.begin();
+		Veiculo veiculo = daoveiculo.read(placa);
+		if (veiculo == null) {
+			throw new Exception("Veículo não encontrado: " + veiculo);
+		}
+		daoveiculo.delete(veiculo);
+		DAO.commit();
+	}
+
+	//TO-DO: ALTERAR VEICULO
+
+	public static List<Veiculo> listarVeiculos() {
+        return daoveiculo.readAll();
+    }
 	
 	private static Date criarData(String data) throws Exception {
 		 String[] DATE_FORMATS = {
@@ -75,44 +107,74 @@ public class Fachada {
 	        // Se nenhum formato for válido, retornar null ou lançar exceção
 	        return null;
 	}
-	public static void criarViagem(String data, String placa, String cnh, String destino) throws Exception {
+
+	public static void cadastrarViagem(String data, String placa, String cnh, String destino) throws Exception {
 		DAO.begin();
+
+		//verifica se o motorista existe 
+		Motorista motorista = daomotorista.read(cnh);
+        if (motorista == null)
+            throw new Exception("Cadastrar viagem - Motorista não encontrado! CNH: " + cnh); 
+		
+		//Formatação da data
 		Date dataConv = criarData(data);
 		if(dataConv == null) {
-			throw new Exception("Criar viagem - Formato de data inválido: "+ data);
+			throw new Exception("Cadastrar viagem - Formato de data inválido: "+ data);
 		}
+
 		String id = Viagem.geraId(dataConv, placa, cnh);
+
+		//Verifica se viagem já existe
 		Viagem viagem = daoviagem.read(id);
 		if(viagem != null) {
 			DAO.rollback();
 			throw new Exception("Criar viagem - Viagem já existe! ID: " + id);
 		}
+
+		//Verifica se o veiculo existe
 		Veiculo veiculo = daoveiculo.read(placa);
 		if(veiculo == null) {
 			DAO.rollback();
-			throw new Exception("Criar viagem - Veiculo não existe! Placa: "+placa);
-		} else {
-			for(Viagem v : veiculo.getViagens()) {
+			throw new Exception("Criar viagem - Veiculo não encontrado! Placa: "+ placa);
+		} 
+
+		// Regra de negócio: não pode haver 2 viagens do mesmo carro na mesma data
+		for(Viagem v : veiculo.getViagens()) {
 				if( v.getData() == dataConv) {
 					DAO.rollback();
 					// REGRA DE NEGÓCIO
-					throw new Exception("Criar Viagem - Não pode haver 2 viagens do mesmo carro na mesma data!\n"+
-					"ID: "+ v.getId());
-				}
-						
+					throw new Exception(String.format("Criar Viagem - Não foi possível cadastrar viagem! O veículo de placa %s já possui viagem nesta data: %s", placa, data));
+				}						
 			}
-		}
-		Motorista motorista = daomotorista.read(cnh);
-		if(motorista == null) {
-			throw new Exception("Criar viagem - Motorista não existe! CNH: "+cnh);
-		}
+		
 		viagem = new Viagem(dataConv, veiculo, motorista, destino);
+		veiculo.adicionar(viagem);
+		motorista.adicionar(viagem);
 		daoviagem.create(viagem);
 		DAO.commit();
 	}
-	
-	
 
+	public static void excluirViagem(String id) throws Exception {
+		DAO.begin();
+		//Verifica se a viagem já existe
+		Viagem viagem = daoviagem.read(id);
+		if (viagem == null) {
+			throw new Exception("Excluir viagem - Essa viagem não existe! \nId: " + id);
+		}
 
+		Veiculo veiculo = viagem.getVeiculo();
+		Motorista motorista = viagem.getMotorista();
+		veiculo.remover(viagem);
+		motorista.remover(viagem);
+		daoveiculo.update(veiculo);
+		daomotorista.update(motorista);
+		daoviagem.delete(viagem);
+		DAO.commit();
+	}
 
+	//TO-DO: ALTERAR VIAGEM
+
+	public static List<Viagem> listarViagens() {
+		return daoviagem.readAll();
+	}
 }
